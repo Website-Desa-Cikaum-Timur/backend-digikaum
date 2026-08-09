9c547598304d1124b4f3b0fdab258d2619e70b8c
Praditya Putra Irawan <rradit885@gmail.com>
2026-08-09 15:33:25 +0700

    fix(storage): add bypass route for local symlink issue and format code via pint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/storage/{path}', function ($path) {
+    $filePath = storage_path('app/public/' . $path);
+
+    if (! File::exists($filePath)) {
+        abort(404);
+    }
+
+    return response()->file($filePath);
+})->where('path', '.*');
