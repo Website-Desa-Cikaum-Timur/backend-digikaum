b089ce5c48f43e2bb9dad1238c23e47d1d961aa4
Praditya Putra Irawan <rradit885@gmail.com>
2026-08-25 14:38:20 +0700

    feat(scheduler): konfigurasi task scheduler untuk otomasi antrean, token, dan pembersihan log

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('queue:work --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping();
+
+Schedule::command('sanctum:prune-expired --hours=24')
+    ->dailyAt('01:00');
+
+Schedule::command('activitylog:clean')
+    ->dailyAt('01:30');
+
+Schedule::command('auth:clear-resets')
+    ->dailyAt('02:00');
